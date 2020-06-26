c0269d826ca8131df0132e7f05bf0ea13617f77a
Dokotela <jgfaulkenberry@gmail.com>
2020-06-25 20:44:18 -0400

    Added then deleted. DB may have to be separate package.

--- lib/database/resource_repository.dart
+++
@@ -1,8 +0,0 @@
-import '../fhir_r4.dart';
-
-abstract class ResourceRepository {
-  Future<int> insertResource(Resource resource);
-  Future updateResource();
-  Future deleteResource();
-  Future<List<Resource>> getAllResources();
-}
